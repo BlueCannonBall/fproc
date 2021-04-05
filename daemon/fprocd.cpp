fc3ea292af5b46ae64cf38e330c543a1545d0795
BlueCannonBall <35404206+BlueCannonBall@users.noreply.github.com>
2021-04-05 08:46:20 -0400

    finished daemon

--- daemon/fprocd.cpp
+++ daemon/fprocd.cpp
@@ -18,9 +18,9 @@ namespace bp = boost::process;
 unsigned int uuid;
 
 struct Process {
-    unsigned int id;
-    unsigned long int pid;
     std::string command;
+    bp::child* child;
+    bool running = false;
 };
 
 mutex data_mtx;
@@ -29,36 +29,101 @@ unordered_map<unsigned int, Process*> processes;
 void handle_conn(int socket) {
     for (;;) {
         spb::StreamPeerBuffer buf(true);
-        read(socket, buf.data_array.data(), 1024);
+        int valread = read(socket, buf.data_array.data(), 1024);
+        if (valread == 0) {
+            cout << "Client disconnected." << endl;
+            return;
+        }
         unsigned char pckt_id = buf.get_u8();
         switch (pckt_id) {
             case 0:
             do {
                 Process *new_proc = new Process;
                 unsigned int id = uuid++;
-                new_proc->id = id;
                 new_proc->command = buf.get_utf8();
                 buf.data_array = std::vector<unsigned char>();
                 buf.offset = 0;
                 buf.put_u8(0);
                 write(socket, buf.data_array.data(), 1);
+                new_proc->child = new bp::child(new_proc->command);
+                data_mtx.lock();
                 processes[id] = new_proc;
-                new_proc->pid = bp::child(new_proc->command).id();
+                new_proc->running = true;
+                data_mtx.unlock();
                 break;
             } while (0);
             case 1:
             do {
                 unsigned int id = buf.get_u32();
-                kill(processes[id]->pid, SIGTERM);
+                data_mtx.lock();
+                if (processes.find(id) == processes.end()) {
+                    buf.data_array = std::vector<unsigned char>();
+                    buf.offset = 0;
+                    buf.put_u8(1);
+                    buf.put_utf8("That process does not exist.");
+                    write(socket, buf.data_array.data(), buf.data_array.size());
+                    data_mtx.unlock();
+                    break;
+                }
+                processes[id]->child->terminate();
                 buf.data_array = std::vector<unsigned char>();
                 buf.offset = 0;
                 buf.put_u8(0);
                 write(socket, buf.data_array.data(), 1);
+                processes[id]->running = false;
+                delete processes[id]->child;
                 delete processes[id];
                 processes.erase(id);
+                data_mtx.unlock();
                 break;
             } while(0);
+            case 2:
+            do {
+                unsigned int id = buf.get_u32();
+                data_mtx.lock();
+                if (processes.find(id) == processes.end()) {
+                    buf.data_array = std::vector<unsigned char>();
+                    buf.offset = 0;
+                    buf.put_u8(1);
+                    buf.put_utf8("That process does not exist.");
+                    write(socket, buf.data_array.data(), buf.data_array.size());
+                    data_mtx.unlock();
+                    break;
+                }
+                processes[id]->child->terminate();
+                buf.data_array = std::vector<unsigned char>();
+                buf.offset = 0;
+                buf.put_u8(0);
+                write(socket, buf.data_array.data(), 1);
+                processes[id]->running = false;
+                data_mtx.unlock();
+                break;
+            } while (0);
+            case 3:
+            do {
+                data_mtx.lock();
+                buf.data_array = std::vector<unsigned char>();
+                buf.offset = 0;
+                buf.put_u32(processes.size());
+                for (auto const& process : processes) {
+                    buf.put_u32(process.first);
+                    buf.put_utf8(process.second->command);
+                    buf.put_u32(process.second->child->id());
+                    buf.put_u8(process.second->running);
+                }
+                data_mtx.unlock();
+            } while (0);
         }
+    }
+}
+
+void maintain_procs() {
+    for (;;) {
+        data_mtx.lock();
+        for (auto const& process : processes) {
+            cout << process.second->child->running() << endl;
+        }
+        data_mtx.unlock();
     }
 }
 
@@ -70,7 +135,7 @@ int main(int argc, char **argv) {
         port = atoi(argv[1]);
     }
 
-    int server_fd, new_socket, valread;
+    int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
@@ -100,6 +165,7 @@ int main(int argc, char **argv) {
     }
 
     cout << "Listening on port " << port << endl;
+    thread(maintain_procs).detach();
     for (;;) {
         if ((new_socket = accept(server_fd, (struct sockaddr*) &address, 
                            (socklen_t*) &addrlen))<0) {
