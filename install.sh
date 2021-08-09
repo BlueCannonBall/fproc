ecde002843441390e0a74b3def5470e1687e3d38
BlueCannonBall <mmttiizz100@gmail.com>
2021-08-09 14:36:20 -0400

    Fix arch bullshit

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ echo "Installing fproc..."
 cp ./daemon/fprocd /usr/local/bin
 cp ./cli/target/release/fproc /usr/local/bin
 cp ./gui/fproc-gui /usr/local/bin
+mkdir -p /usr/local/share/applications
 cp ./gui/fproc.desktop /usr/local/share/applications
+mkdir -p /usr/local/share/icons/hicolor/48x48/apps
 cp icon.png /usr/local/share/icons/hicolor/48x48/apps/fproc.png
 
 echo "Successfully installed fproc!"
